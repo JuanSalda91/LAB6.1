39a0b47b00abf4a0589c263febe423f3546b0efb
JuanSalda91 <juansalda9131@gmail.com>
2025-11-22 22:25:58 -0500

    product base class modified

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,46 +1,17 @@
-// class Product {
-//     sku: string;
-//     name: string;
-//     price: number;
+// ----- Creating product base class ----- //
 
-//     constructor(sku: string, name: string, price: number) {
-//         this.sku = sku;
-//         this.name = name;
-//         this.price = price;
-//     }
+export class Product {
+    constructor (
+        public sku: string,
+        public name: string,
+        public price: number,
+    ) {}
 
-//     displayDetails(): string {
-//         return `Product sku: ${this.sku}, Product name: ${this.name}, Product price before taxes: $${this.price}.`
-//     }
-// };
+    displayDetails(): string {
+        return `SKU: ${this.sku}, Name: ${this.name}, Price $${this.price.toFixed(2)}`;
+    }
 
-// const product1 = new Product("akjdnka646648", "myProduct", 650);
-// console.log(product1.displayDetails())
-
-// class Product {
-//   public static taxRate = 0.05;
- 
-//   private sku: string;
-//   public name: string;
-//   protected _price: number;
- 
-//   constructor(sku: string, name: string, price: number) {
-//     this.sku = sku;
-//     this.name = name;
-//     this._price = price;
-//   }
- 
-//   get price(): number {
-//     return this._price * (Product.taxRate + 1);
-//   }
-//   set price(newBasePrice: number) {
-//     this._price = newBasePrice;
-//   }
- 
-//   public displayDetails(): string {
-//     return `${this.name} (SKU: ${this.sku}) costs $${this.price}.`;
-//   }
-// }
-
-// const product1 = new Product("dsfsd65", "myProduct", 500);
-// console.log(product1.displayDetails());
+    getPriceWithTax(): number {
+        return this.price
+    }
+}
